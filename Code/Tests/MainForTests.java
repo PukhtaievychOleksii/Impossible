91b81ce8e985a8f51702c66cf968445468ce5324
Diana <birukovad50@gmail.com>
2023-11-21 20:47:24 +0100

    CorrectedTest+MainForTests

---
+++ Code/Tests/MainForTests.java
@@ -0,0 +1,9 @@
+package Code.Tests;
+
+public class MainForTests {
+    public static void main(String[] args) {
+        FloorTest test = new FloorTest();
+        test.setUp();
+         test.test_tiles();
+    }
+}
