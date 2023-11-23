c72a1e8289fc6059c98ff80876d885c1300a4753
unknown <pukhtaievych.oleksii@gmail.com>
2023-11-23 18:04:44 +0100

    WallLineUpdate

--- Code/WallLine.java
+++ Code/WallLine.java
@@ -1,25 +1,31 @@
 package Code;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Optional;
 
 public class WallLine {
     private ArrayList<Tile> tileTypesSequence;
     private ArrayList<Optional<Tile>> presentTiles;
-    private WallLine upWall;
-    private WallLine downWall;
+    private Optional<WallLine> upWall;
+    private Optional<WallLine> downWall;
 
     public WallLine(ArrayList<Tile> tileTypesSequence){
         this.tileTypesSequence = tileTypesSequence;
         presentTiles = new ArrayList<>();
+        for(int i = 0; i < tileTypesSequence.size(); i++){
+            presentTiles.add(Optional.empty());
+        }
+        upWall = Optional.empty();
+        downWall = Optional.empty();
     }
 
     public void setUpWall(WallLine upWall){
-        this.upWall = upWall;
+        this.upWall = Optional.of(upWall);
     }
 
     public void setDownWall(WallLine downWall){
-        this.downWall = downWall;
+        this.downWall = Optional.of(downWall);
     }
 
     public boolean canPutTile(Tile tile){
@@ -38,9 +44,8 @@ public class WallLine {
     }
 
     public Points putTile(Tile tile){
-        //TODO: Points calculation
-        Points points = new Points(0);
-
+        int points = 0;
+        //put
         int index = 0;
         for(int i = 0; i < tileTypesSequence.size(); i++){
             if(tile == tileTypesSequence.get(i)){
@@ -49,9 +54,27 @@ public class WallLine {
             }
         }
         presentTiles.set(index, Optional.of(tile));
+        //count
+        int horSeq = 1;
+        int vertSeq = 1;
+        if(index - 1 >= 0 && !presentTiles.get(index - 1).isEmpty()) horSeq++;
+        if(index + 1 < presentTiles.size() && !presentTiles.get(index + 1).isEmpty()) horSeq++;
+        if(upWall.isPresent()){
+            ArrayList<Optional<Tile>> upLine = upWall.get().getTiles();
+            if(upLine.get(index).isPresent()) vertSeq++;
+        }
+        if(downWall.isPresent()){
+            ArrayList<Optional<Tile>> downLine = downWall.get().getTiles();
+            if(downLine.get(index).isPresent()) vertSeq++;
+        }
 
-        return points;
+        if(horSeq == 1 && vertSeq == 1) points = 1;
+        //TODO: FIX Points calculation to full
+
+        return new Points(points);
     }
+
+
 
     public String state(){
         String toReturn = "";
