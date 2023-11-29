9f0a0d0a178667650a1896704b87b641e8a0ae68
Diana <birukovad50@gmail.com>
2023-11-29 16:13:05 +0100

    Test+FixConstructor

--- Code/Tests/FactoryTest.java
+++ Code/Tests/FactoryTest.java
@@ -28,8 +28,23 @@ public class FactoryTest {
 
         ArrayList<Tile> tiles = new ArrayList<>(Arrays.asList(Tile.RED, Tile.RED, Tile.BLACK, Tile.BLACK));
         Factory factory1 = new Factory(tiles);
-        assertEquals("Two red tiles should be taken", 2, factory1.take(0).size());
+        assertEquals("Two red tiles should be taken.", 2, factory1.take(0).size());
         assertEquals("Two green left.", 2, factory1.sizeOfFactory());
+
+        assertEquals("Two green tiles should be taken.", 2, factory1.take(3).size());
+        assertEquals("No tiles left", 0, factory1.sizeOfFactory());
+
+        tiles = new ArrayList<>(Arrays.asList(Tile.BLUE, Tile.BLUE, Tile.BLUE, Tile.BLUE));
+        Factory factory2 = new Factory(tiles);
+        assertEquals("Four black tiles should be taken.", 4, factory2.take(0).size());
+        assertEquals("No tiles left.", 0, factory2.sizeOfFactory());
+
+        tiles = new ArrayList<>(Arrays.asList(Tile.YELLOW, Tile.YELLOW, Tile.YELLOW, Tile.GREEN));
+        Factory factory3 = new Factory(tiles);
+        assertEquals("Only one green tile should be taken.", 1, factory2.take(3).size());
+        assertEquals("Three should be left.", 3, factory2.sizeOfFactory());
+        assertEquals("Three yellow tiles should be taken.", 3, factory2.take(0).size());
+        assertEquals("No tiles left.", 0, factory2.sizeOfFactory());
 
 
     }
